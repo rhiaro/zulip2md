95e9b2fcd18e5748fe751a0ae125d8c87267919e
rhiaro <amy@rhiaro.co.uk>
2018-04-24 19:55:28 +0200

    Output minutes more or less okay

--- zulip2md/zulip2md.py
+++ zulip2md/zulip2md.py
@@ -1,21 +1,129 @@
+from collections import OrderedDict
 import argparse
 import json
 
 
-def get_topic(json, topic):
-    pass
+def get_topic(jason, topic):
+    timestream = {}
+    for message in jason:
+        if message["subject"] == topic:
+            timestream[message["timestamp"]] = message
+
+    data = OrderedDict(sorted(timestream.items()))
+    return data
 
 
-def to_markdown(json):
-    pass
+def get_scribed_block(first_message, data):
+    content = first_message["content"].split("** says")
+    if len(content) > 1:  # TODO: fixed when the regex is in
+        text = content[1].strip()
+        author = content[0].replace("@**", "")
+
+        # Sniff subsequent messages from the same speaker
+        # afters = []
+        # for message in data.values():
+        #     if message["content"].startswith("@**"):
+        #         continue
+        #     if message["timestamp"] > first_message["timestamp"] and message["content"].startswith(".."):
+        #         text = "%s\n%s" % (text, message["content"])
+
+        return text, author
+    else:
+        return None, None
 
 
-def write(markdown):
-    pass
+def to_markdown(data):
+    markdown = ""
+    attributed = OrderedDict()
+
+    attendees = []
+    proposals = []
+    resolutions = []
+    scribes = []
+    for message in data.values():
+        # Get attendees
+        if "present+" in message["content"]:
+            attendees.append(message["sender_full_name"])
+        # Get proposals
+        if "PROPOSAL:" in message["content"] or "PROPOSED:" in message["content"]:
+            proposals.append(message["content"])
+        # Get resolutions
+        if "RESOLUTION:" in message["content"] or "RESOLVED:" in message["content"]:
+            resolutions.append(message["content"])
+
+        # Find the scribe(s)
+        if "scribe: " in message["content"].lower():
+            scribe = message["content"].lower().split("scribe: ")[1]
+            if "\n" in scribe:
+                scribe = scribe.split("\n")[0]
+            scribe = scribe.replace("@", "")
+            scribe = scribe.replace("*", "")
+            scribe = scribe.strip()
+            scribes.append(scribe)
+
+        # Format scribe messages
+        # TODO replace this with regex /@\*\*([^*]*)\*\*/ says
+        if message["content"].startswith("@**"):
+            text, author = get_scribed_block(message, data)
+            if text is not None:
+                attributed[message["timestamp"]] = {
+                    "time": message["timestamp"],
+                    "author": author,
+                    "content": text,
+                    "scribed": True
+                }
+
+        elif message["content"].startswith(".."):
+            attributed[message["timestamp"]] = {
+                "time": message["timestamp"],
+                "author": None,
+                "content": message["content"],
+                "scribed": True
+            }
+        else:
+            # Inline interjections
+            attributed[message["timestamp"]] = {
+                "time": message["timestamp"],
+                "author": message["sender_full_name"],
+                "content": message["content"],
+                "scribed": False
+            }
+            # TODO: format these differently
+
+        # TODO: Capture emoji reactions to messages
+
+    # Time sort
+    for k, v in attributed.items():
+        if v["author"] is not None:
+            print(v["time"])
+            print(v["author"])
+        if not v["scribed"]:
+            print(">>>> %s", v["content"])
+        else:
+            print(v["content"])
+
+    # Glue together
+
+    return markdown
 
 
-def convert(json, topic):
-    pass
+def write(markdown, outfile):
+    with open(outfile) as file:
+        pass
+
+
+def convert(args):
+
+    infile = args.inf
+    outfile = args.out
+    topic = args.topic
+
+    with open(infile) as f:
+        jason = json.load(f)
+
+    data = get_topic(jason, topic)
+    markdown = to_markdown(data)
+    write(markdown, outfile)
 
 
 def cli():
@@ -26,13 +134,8 @@ def cli():
     parser.add_argument("--topic", help="Topic to extract from stream")
 
     args = parser.parse_args()
-    infile = args.inf
-    outfile = args.out
-    topic = args.topic
 
-    print infile
-    print outfile
-    print topic
+    convert(args)
 
 
 if __name__ == '__main__':
